cab833e17385cc05437096aca421914ce8c5c49a
takahams0126 <61419190+takahams0126@users.noreply.github.com>
2026-09-18 15:32:06 +0900

    viewer: bump version for unified flow icons

--- presentation/main.js
+++ presentation/main.js
@@ -5,7 +5,7 @@
 const qs = new URLSearchParams(location.search);
 const type = qs.get('type');
 const pageType = type || 'home';
-const PRESENTATION_VERSION = '20260918-shared-semantics-1';
+const PRESENTATION_VERSION = '20260918-shared-flow-icons-2';
 
 const body = document.body;
 body.classList.add(`page-${pageType}`);
